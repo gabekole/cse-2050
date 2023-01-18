8ebfc73f0e86e0f1fdfaa2f3c2eb63858ca78638
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-01-18 15:25:01 +0000

    prototype

--- letters.py
+++ letters.py
@@ -1,8 +1,22 @@
+from string import ascii_letters
+
 def letter_count(file):
     count_map = {}
     char = file.read(1)
+
+
+
     while char:
-        process(char)
+        if not char in ascii_letters:
+            continue
+ 
+        lower_case = char.lower()
+        
+        if not lower_case in count_map.keys():
+            count_map[lower_case] = 1
+        else:
+            count_map[lower_case] += 1
+
         char = file.read(1)
 
     file.close()
