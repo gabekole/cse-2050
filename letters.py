281e1cf1aec4c673ca0f83e7905e9ca48c962b6d
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-01-19 16:29:30 +0000

    iterate over lines

--- letters.py
+++ letters.py
@@ -1,24 +1,19 @@
 from string import ascii_letters
 
 def letter_count(filename):
-    file = open(filename, 'r')
     count_map = {}
-    char = file.read(1)
-    while char:
-        if not char in ascii_letters:
-            char = file.read(1)
-            continue
+    with open(filename, mode='r', encoding='utf-8') as file:
+        for line in file:
+            for char in line:
+                if not char in ascii_letters:
+                    continue
  
-        lower_case = char.lower()
+                lower_case = char.lower()
         
-        if not lower_case in count_map:
-            count_map[lower_case] = 1
-        else:
-            count_map[lower_case] += 1
-
-        char = file.read(1)
-
-    file.close()
+                if not lower_case in count_map:
+                    count_map[lower_case] = 1
+                else:
+                    count_map[lower_case] += 1
 
     return count_map
         
