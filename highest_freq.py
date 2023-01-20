b7929830dc17ed93a8f6cb375ec398111d7cdf0f
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-01-20 20:06:38 +0000

    Add more comments

--- highest_freq.py
+++ highest_freq.py
@@ -1,11 +1,27 @@
 from letters import letter_frequency, letter_count
 
 def highest_freq(filename):
+    """
+    Returns a tuple containing the most frequent ascii letter 
+    in the provided file and its relative frequency
+
+    Input: path to file to be parsed
+    Output: Tuple of the form: (letter, frequency)
+
+    Note: Counting is case insensitive
+    """
+    
+    # Gets the total count of the ascii letters
     counts = letter_count(filename)
+
+    # Convert the occurance count to the frequency
     frequency = letter_frequency(counts)
 
+    # Sort dictionary items by frequency and convert to a list
     res = sorted(frequency.items(), key = lambda x:x[1], reverse=True)
 
+    # Return the first item (highest frequency), 
+    # if there is no first item, return ('a', 0)
     try:
         return res[0]
     except IndexError:
