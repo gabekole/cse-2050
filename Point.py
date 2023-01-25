d1e09680b31679b658df09dba3ec928413a4c8a4
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-01-25 14:34:14 +0000

    :sparkles: add starter code

---
+++ Point.py
@@ -0,0 +1,30 @@
+
+
+# ^^^Implement class and functionality above (remember to include docstrings!)
+# vvvImplement tests below
+
+if __name__ == '__main__':
+    # All tests should use `assert`, not `print`
+    
+    ##### test init #####
+    # assert correct x
+    # assert correct y
+
+    ##### test lt #####
+    # Expected True (e.g `p1 < p2`)
+    # Expected False (e.g. `not p1 < p2`)
+
+    ##### test gt #####
+    # Expected True (e.g `p1 > p2`)
+    # Expected False (e.g. `not p1 > p2`)
+
+    ##### test eq #####
+    # Expected True (e.g `p1 == p2`)
+    # Expected False (e.g. `not p1 == p2`)
+
+    
+    ##### test str #####
+    # assert str(some_point) == expected_string
+
+    ##### test dist_from_origin() #####
+   
