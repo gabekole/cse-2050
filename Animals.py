66d4e374ef1188e3fdd964ec96083d8110fc159e
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-01-25 23:12:03 +0000

    Start out

---
+++ Animals.py
@@ -0,0 +1,45 @@
+class Animal:
+  def __init__(self, name):
+    self.name = name
+    
+  def speak(self):
+    return f'{self.name} says hi'
+    
+  def reply(self):
+    self.speak()
+
+
+class Mammal(Animal):
+  def __init__(self, name):
+    super().__init__(name)
+    
+  def speak(self):
+    return f'{self.name} says hi'
+
+
+class Cat(Mammal):
+  def __init__(self, name):
+    super().__init__(name)
+    
+  def speak(self):
+    return f'{self.name} says Meow!'
+
+class Dog(Mammal):
+  def __init__(self, name):
+    super().__init__(name)
+    
+  def speak(self):
+    return f'{self.name} says Woof!'
+
+
+class Primate(Mammal):
+  def __init__(self, name):
+    super().__init__(name)
+    
+  def speak(self):
+    return f'{self.name} says Eek!'
+
+
+class ComputerScientist(Primate):
+  def __init__(self, name):
+    super().__init__(name)
