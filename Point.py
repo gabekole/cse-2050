1bb0e1ab406da145338da9b799af550ab21dadcd
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-01-25 15:10:48 +0000

    :zap: refactor distance function :fire: :fire:

--- Point.py
+++ Point.py
@@ -12,7 +12,7 @@ class Point:
         return (self.x**2 + self.y**2)**0.5
 
     def __str__(self):
-        return f'({self.x}, {self.y})'
+        return '('+ str(self.x) + ', ' + str(self.y) + ')'
     
 
 # ^^^Implement class and functionality above (remember to include docstrings!)
