1280826362e4f1300791aaba36042aff4a69ae56
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-01-25 15:06:18 +0000

    :sparkles: good

--- Point.py
+++ Point.py
@@ -2,7 +2,14 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
+    def __gt__(self, other):
+        return isinstance(other, Point) and self.dist_from_origin() > other.dist_from_origin()
+    def __lt__(self,other):
+        return isinstance(other, Point) and self.dist_from_origin() < other.dist_from_origin()
+    def __eq__(self,other):
+        return isinstance(other, Point) and self.dist_from_origin() == other.dist_from_origin()
+    def dist_from_origin(self):
+        return (self.x**2 + self.y**2)**0.5
     
 
 # ^^^Implement class and functionality above (remember to include docstrings!)
@@ -12,24 +19,48 @@ if __name__ == '__main__':
     # All tests should use `assert`, not `print`
     
     ##### test init #####
+    p1 = Point(3, 4)
+    p2 = Point(-6, 8)
+    p3 = Point(-6, 8)
+    p4 = Point(-3, -4)
+
     # assert correct x
+    assert p1.x == 3
+    assert p2.x == -6
+    assert p3.x == -6
+    assert p4.x == -3
+
     # assert correct y
+    assert p1.y == 4
+    assert p2.y == 8
+    assert p3.y == 8
+    assert p4.y == -4
 
     ##### test lt #####
-    # Expected True (e.g `p1 < p2`)
-    # Expected False (e.g. `not p1 < p2`)
+    assert p1 < p2 # Expected True (e.g `p1 < p2`)
+    assert not p2 < p1 # Expected False (e.g. `not p1 < p2`)
+    assert not p2 < p3 # Expected False (same)
+    assert not p1 < p4 # Expected false (same distance)
 
     ##### test gt #####
-    # Expected True (e.g `p1 > p2`)
-    # Expected False (e.g. `not p1 > p2`)
+    assert p2 > p1 # Expected True (e.g `p1 > p2`)
+    assert p1 > p2 # Expected False (e.g. `not p1 > p2`)
+    assert p2 > p1 # Expected true
+    assert p3 > p4 # Expected true, greater distance
+    assert not p3 > p2 # Expected false since same disatance
 
     ##### test eq #####
-    # Expected True (e.g `p1 == p2`)
-    # Expected False (e.g. `not p1 == p2`)
+    assert p1 == p4 # Expected True (e.g `p1 == p2`)
+    assert p2 == p3 # Expected True (same distance)
+    assert p1 != p2 # Expected true (e.g. `not p1 == p2`)
+    assert p2 != p4 # Expected true since different
 
     
     ##### test str #####
-    # assert str(some_point) == expected_string
+    assert str(p1) == "(3, 4)"
+    assert str(p2) == "(-6, 8)"
+    assert str(p3) == "(-6, 8)"
+    assert str(p4) == "(-3, -4)"
 
     ##### test dist_from_origin() #####
-    pass
+    assert round(p1.dist_from_origin(),2) == 7.21110
