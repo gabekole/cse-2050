d5784b1093da64228135aa21c6f2e3fc2992ea94
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-02-01 15:22:22 +0000

    :smile: complete implementation of both functions

--- hw3.py
+++ hw3.py
@@ -1,4 +1,15 @@
 def find_pair_naive(l1, target):
+    """
+    Returns a set of items in the list that sum to the target
+            
+    Args:
+        l1 (list): the list to find pairs in.
+        target (number): the summation target.
+
+    Note:
+        Utilizes a slow naive approach
+    """
+
     result = set()
 
     for x in l1:
@@ -9,4 +20,25 @@ def find_pair_naive(l1, target):
     return result
 
 def find_pairs_optimized(l1, target):
+    """
+    Returns a set of items in the list that sum to the target
+            
+    Args:
+        l1 (list): the list to find pairs in.
+        target (number): the summation target.
+    """
+
+    history_set = set() # Set to store past numbers
+
+    result_set = set()
+
+    for current_num in l1:
+        compliment = target - current_num
+
+        if compliment in history_set:
+            
+            result_set.add( compliment, current_num )
+
+        history_set.add( current_num )
     
+    return result_set
