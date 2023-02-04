5eb61c59434387c87932aaee892134ac7bc81943
gabekole <gabriel.koleszar@uconn.edu>
2023-02-04 14:05:00 -0500

    :memo: add time complexity to documentation

--- hw3.py
+++ hw3.py
@@ -13,14 +13,16 @@ def find_pairs_naive(l1, target):
         Utilizes a slow naive approach
     """
 
-    result = set()
+    result = set()                      # 2
 
-    for idx, x in enumerate(l1):
-        for y in l1[idx:]:
-            if x + y == target:
-                result.add( (x, y) )
+    for idx, x in enumerate(l1):        # n
+        for y in l1[idx:]:              # n / 2 (on average since number of calls ranges from n to 0)
+            if x + y == target:         # 2 (add, then compare)
+                result.add( (x, y) )    # 2 (create tuple, add to set)
 
-    return result
+    return result                       # 1
+                                        #----------------------
+                                        # 2 + n(n/2(2 + 2)) + 1 = O(n^2)
 
 def find_pairs_optimized(l1, target):
     """
@@ -31,20 +33,22 @@ def find_pairs_optimized(l1, target):
         target (number): the summation target.
     """
 
-    history_set = set() # Set to store past numbers
+    history_set = set() # Set to store past numbers     # 2 (create, then assign)
 
-    result_set = set()
+    result_set = set()                                  # 2 (create, then assign)
 
-    for current_num in l1:
-        compliment = target - current_num
+    for current_num in l1:                              # n
+        compliment = target - current_num               # 2 (subtract, then assign)
 
-        if compliment in history_set:
+        if compliment in history_set:                   # 1 (constant time set lookup)
             
-            result_set.add( (compliment, current_num) )
+            result_set.add( (compliment, current_num) ) # 2 (create, then assign)
 
-        history_set.add( current_num )
+        history_set.add( current_num )                  # 1 (add to set)
     
-    return result_set
+    return result_set                                   # 1
+                                                        #----------------------------
+                                                        # 2 + 2 + n(2 + 1 + 2 + 1) + 1 = O(n)
 
 
 def measure_min_time(func, args):
@@ -58,29 +62,29 @@ def measure_min_time(func, args):
     fastest_trial = float('inf')
 
     for _ in range(n_trials):
-        start_time = time.time()
+        start_time = time.perf_counter()
 
         func(*args)
 
-        end_time = time.time()
-        runtime_in_microseconds = (end_time - start_time) * 1000
+        end_time = time.perf_counter()
+        runtime_in_miliseconds = (end_time - start_time) * 1000
 
-        fastest_trial = min(runtime_in_microseconds, fastest_trial)
+        fastest_trial = min(runtime_in_miliseconds, fastest_trial)
 
     return fastest_trial
 
+if __name__ == "__main__":
+    print ("{:<15} {:<35} {:<35}".format('n','naive','optimized'))
+    print("*"*85)
+    for n in [10, 50, 100, 150, 200, 300, 500]:
+        
+        random.seed(1)
+        n_size_list = random.sample(range(1, 600), n)
+        random_target = random.randint(50, 400)
 
-# print ("{:<15} {:<15} {:<15}".format('n','naive','optimized'))
-# print("*"*45)
-# for n in [10, 50, 100, 150, 200, 300, 500]:
-    
-#     random.seed(1)
-#     n_size_list = random.sample(range(1, 600), n)
-#     random_target = random.randint(50, 400)
+        naive_result = measure_min_time(find_pairs_naive, (n_size_list, random_target))
+        optimized_result = measure_min_time(find_pairs_optimized, (n_size_list, random_target))
 
-#     naive_result = measure_min_time(find_pairs_naive, (n_size_list, random_target))
-#     optimized_result = measure_min_time(find_pairs_optimized, (n_size_list, random_target))
-
-#     print("{:<15} {:<15} {:<15}".format(n, f'{naive_result:.4f}', f'{optimized_result:.4f}' ))
+        print("{:<15} {:<35} {:<35}".format(n, f'{naive_result:.4f}', f'{optimized_result:.4f}' ))
 
     
