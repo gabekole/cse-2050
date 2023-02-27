10e98cad8b8221ef450bb4ca402b39a712051c1e
gabekole <gabriel.koleszar@uconn.edu>
2023-02-26 20:42:32 -0500

    :smile: write tests and implementation

--- TestSolvePuzzle.py
+++ TestSolvePuzzle.py
@@ -4,14 +4,31 @@ import unittest
 class TestSolvePuzzle(unittest.TestCase):
         def testClockwise(self):
                 """Tests a board solveable using only CW moves"""
+                # self.assertTrue(puzzle([1, 0]))
+                # self.assertTrue(puzzle([2, 0, 0]))
+                # self.assertTrue(puzzle([1, 1, 3]))
+                # self.assertTrue(puzzle([1, 2, 1, 2]))
 
         def testCounterClockwise(self):
                 """Tests a board solveable using only CCW moves"""
+                self.assertTrue(puzzle([1, 0]))
+                self.assertTrue(puzzle([1, 0, 0]))
+                self.assertTrue(puzzle([1, 2, 5, 1, 2, 1, 0]))
 
         def testMixed(self):
                 """Tests a board solveable using only a combination of CW and CCW moves"""
+                self.assertTrue(puzzle([5, 3, 1, 2, 4, 2, 1, 0]))
+                self.assertTrue(puzzle([2, 4, 1, 24, 0, 9]))
+                self.assertTrue(puzzle([4, 1, 2, 3, 4]))
+                self.assertTrue(puzzle([49, 1, 5, 9, 10]))
+                
+                
         
         def testUnsolveable(self):
                 """Tests an unsolveable board"""
+                self.assertFalse(puzzle([7, 1, 2, 3, 2, 9, 99]))
+                self.assertFalse(puzzle([6, 0, 0, 1, 2, 5]))
+                self.assertFalse(puzzle([5, 4, 1, 2, 0, 0, 0, 8, 10]))
+                self.assertFalse(puzzle([0, 0]))
 
 unittest.main()
--- solve_puzzle.py
+++ solve_puzzle.py
@@ -1,8 +1,23 @@
-def solve_puzzle(): # Make sure to add input parameters here
+def solve_puzzle(arr): # Make sure to add input parameters here
     """Returns True(False) if a given board is (is not) solveable"""
+    last_index = len(arr) - 1
 
-    # 1) Base case: have you found a valid solution?
+    visited = set()
 
-    # 2) Find all valid next-steps
+    def solve_inner(current_index):
+        if current_index == last_index:
+            return True
+        
+        if current_index in visited:
+            return False
+        
+        visited.add(current_index)
 
-    # 3) Recursively explore next-steps, returning True if any valid solution is found
+        step_size = arr[current_index]
+
+        right_index = (current_index + step_size) % len(arr)
+        left_index = (current_index - step_size) % len(arr)
+        
+        return solve_inner(left_index) or solve_inner(right_index)
+    
+    return solve_inner(0)
