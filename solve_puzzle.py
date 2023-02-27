bc7c685277644d45065e9ab5a205b10112399a56
gabekole <gabriel.koleszar@uconn.edu>
2023-02-26 19:47:13 -0500

    :sparkles: add starter files

---
+++ solve_puzzle.py
@@ -0,0 +1,8 @@
+def solve_puzzle(): # Make sure to add input parameters here
+    """Returns True(False) if a given board is (is not) solveable"""
+
+    # 1) Base case: have you found a valid solution?
+
+    # 2) Find all valid next-steps
+
+    # 3) Recursively explore next-steps, returning True if any valid solution is found
