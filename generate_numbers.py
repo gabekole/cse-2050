a348a3cbc55f251b7c7537e415ed4e62a82bdee8
gabekole <gabriel.koleszar@uconn.edu>
2023-03-14 00:54:47 -0400

    updaten umber count

--- generate_numbers.py
+++ generate_numbers.py
@@ -2,9 +2,8 @@
 import random
 
 ##### Generate list of numbers #####
-n = 500 # Max is 2000 due to memory constraints with quicksort
+n = 2000 # Max is 2000 due to memory constraints with quicksort
 L1 = [random.randint(0, n) for i in range(n)]
-L2 = [random.randint(0, n) for i in range(n)]
 
 ##### Create file to write to #####
 f = open(f"./numbers.txt", "w")
