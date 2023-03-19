483d020064986161b0fbb8f6b04b551d2c29a365
gabekole <gabriel.koleszar@uconn.edu>
2023-03-18 22:28:09 -0400

    update name

--- test_time.py
+++ test_time.py
@@ -2,10 +2,17 @@ import string
 import random
 import time
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import savgol_filter
 
 from epic import linear_string, quadratic_string, n_log_n_string, find_substring
 
 vowels = set('aeiou')
+
+
+window_size = 1100
+poly_order = 1
+max_string_size = 5000
 
 def valid_substring(s):
     vowel_index = None
@@ -38,7 +45,7 @@ def run_tests():
     linear_run_times = []
     linear_n_count = []
 
-    for N in range(2, 4000):
+    for N in range(2, max_string_size):
         print(N)
 
         random_string = make_valid_string(N)
@@ -52,31 +59,13 @@ def run_tests():
         linear_n_count.append(N)
         linear_run_times.append(end-start)
     
-    plt.plot(linear_n_count, linear_run_times, color='blue')
 
-
-
-    quadratic_run_times = []
-    quadratic_n_count = []
-    for N in range(2, 500):
-        print(N)
-
-        random_string = make_valid_string(N)
-
-        start = time.perf_counter_ns()
-
-        quadratic_string(random_string)
-
-        end = time.perf_counter_ns()
-
-        quadratic_n_count.append(N)
-        quadratic_run_times.append(end-start)
-
-    plt.plot(quadratic_n_count, quadratic_run_times, color='orange')
+    y_smooth = savgol_filter(linear_run_times, window_size, poly_order)
+    plt.plot(linear_n_count, y_smooth, color='black')
 
     n_log_run_times = []
     n_log_n_count = []
-    for N in range(2, 4000):
+    for N in range(2, max_string_size):
         print(N)
 
         random_string = make_valid_string(N)
@@ -90,12 +79,13 @@ def run_tests():
         n_log_n_count.append(N)
         n_log_run_times.append(end-start)
 
-    plt.plot(n_log_n_count, n_log_run_times, color='green')
+    y_smooth = savgol_filter(n_log_run_times, window_size, poly_order)
+    plt.plot(n_log_n_count, y_smooth, color='green')
 
 
     run_times = []
     n_count = []
-    for N in range(2, 1200):
+    for N in range(2, max_string_size):
         print(N)
 
         random_string = make_valid_string(N)
@@ -109,8 +99,8 @@ def run_tests():
         n_count.append(N)
         run_times.append(end-start)
 
-    plt.plot(n_count, run_times, color='red')
-
+    y_smooth = savgol_filter(run_times, window_size, poly_order)
+    plt.plot(n_count, y_smooth, color='red')
     plt.show()
 
 
