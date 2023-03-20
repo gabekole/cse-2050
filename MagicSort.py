933f0fae9af56615c653d42b565ba510196e1017
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-03-20 22:44:07 +0000

    :sparkles: add starter file

---
+++ MagicSort.py
@@ -0,0 +1,14 @@
+from math import log2
+
+
+def linear_scan(): pass
+
+def reverse_list(): pass
+
+def insertionsort(): pass
+
+def quicksort(): pass
+
+def mergesort(): pass
+   
+def magic_sort(): pass
---
+++ TestMagicSort.py
@@ -0,0 +1,20 @@
+import unittest
+from MagicSort import linear_scan, reverse_list, insertionsort, quicksort, mergesort,  magic_sort
+
+
+class Test_linear_scan(unittest.TestCase): pass
+
+class Test_reverse_list(unittest.TestCase): pass
+
+
+
+class Test_insertionsort(unittest.TestCase): pass
+
+class Test_quicksort(unittest.TestCase): pass
+
+class Test_mergesort(unittest.TestCase): pass
+
+class Test_magicsort(unittest.TestCase): pass
+
+
+unittest.main()
